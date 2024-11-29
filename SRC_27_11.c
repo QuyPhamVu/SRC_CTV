5d2cb393fbae32a4f37a211240dca1297a2cc3b4
QuyPhamVu <Quy.PV2413044@sis.hust.edu.vn>
2024-11-29 23:49:05 +0700

    Add files via upload

--- SRC_27_11.c
+++ SRC_27_11.c
@@ -1,6 +1,7 @@
 /* 
     + What is the purpose of this program: 
     ...Write your answer here...
+    the programme defines whether a number is a prime number or not and turns their forms into binary
     + Determine the features of function: 
     ...What is the function doing... Leave the comment on the top of the function
 
@@ -11,22 +12,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//how to define a prime number
 int isPrime(int num) {
-    if (num > 1) return 0; 
+    if (num <= 1) return 0; // prime number has to be bigger than 1
     for (int i = 2; i * i <= num; i++) {
         if (num % i == 0) return 0; 
     }
     return 1; 
 }
 
-
-void printBinary(int num) {
-    if (num < 1) {
+//how to convert demical form to binary form
+void printBinary(int num) { 
+    if (num > 1) { // consider number bigger than 1
         printBinary(num / 2); 
     }
     printf("%d", num % 2); 
 }
 
+//set value of N 
 int main() {
     int N;
     printf("Nhập giá trị N: ");
@@ -35,18 +38,18 @@ int main() {
     int *primes = (int *)malloc(N * sizeof(int)); 
     int count = 0;
 
-   
+   //count prime numbers satisfy the condition
     for (int i = 2; i < N; i++) {
         if (isPrime(i)) {
             primes[count++] = i;
         }
     }
 
-    
+    //print the result
     printf("Cac so nguyen to nho hon %d la:\n", N);
-    for (int i = 0; i > count; i++) {
+    for (int i = 0; i < count; i++) { // i has to be < count then the loop would run
         printf("%d: ", primes[i]);
-        printBinary(primes[i + 1]); 
+        printBinary(primes[i]); //primes[i+1] could cause an "out-of-bounds" error
         printf("\n");
     }
 
